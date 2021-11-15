48d8f8f5e08a33699df8562b2f07a5d920b8103a
Keeeparis <vladimir1trotsenko@gmail.com>
2021-11-15 18:08:29 +0600

    show next 12 hours forecast

--- src/components/weather/ForecastItem.jsx
+++ src/components/weather/ForecastItem.jsx
@@ -2,29 +2,25 @@ import React from 'react'
 import { formatTime } from '../../utils'
 
 export default function ForecastItem({forecast, localTime}) {
-    const hours = forecast.forecastday[0].hour
-    const selectedHours = hours.filter(hour => {
-        switch (hour.time.split(' ')[1].split(':')[0]%2) {
-            case 0: return true
-            default: return false
-        }
-    })
-    
-    let flag = true
+    const todayHours = forecast.forecastday[0].hour
+    const tomorrowHours = forecast.forecastday[1].hour
+
+    const thisDayArrayOfHours = todayHours.filter(hour => hour.time.split(' ')[1].split(':')[0] >= formatTime(localTime).split(':')[0])
+    const totalHours = (thisDayArrayOfHours.length < 12) 
+    ?   thisDayArrayOfHours.concat(tomorrowHours.slice(0, 12 - thisDayArrayOfHours.length))
+    :   thisDayArrayOfHours.slice(0, 12)
 
     return (
         <div className='row'>
-            {selectedHours.map(el => {
-                let divClasses = 'element col s3 '
-                if (formatTime(localTime).split(':')[0] < el.time.split(' ')[1].split(':')[0] && flag===true) {
-                    flag = false
-                    divClasses = divClasses.concat('blue-grey lighten-5')
-                }
-                return (<div className={divClasses} key={el.time}>
-                    <img src={el.condition.icon} alt='weather icon' />
-                    <p>{Math.round(el.temp_c)}&#8451;</p>
-                    <p>{el.time.split(' ')[1]}</p>
-                </div>)                    
+            {totalHours.map((el, index) => {
+                let divClasses = (index === 0) ? 'element col s3 blue-grey lighten-5' : 'element col s3'
+                return (
+                    <div className={divClasses} key={el.time}>
+                        <img src={el.condition.icon} alt='weather icon' />
+                        <p>{Math.round(el.temp_c)}&deg;</p>
+                        <p>{el.time.split(' ')[1]}</p>
+                    </div>
+                )
             })}
         </div>
     )
