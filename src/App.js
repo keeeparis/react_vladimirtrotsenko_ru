69fd31d1b92de4c3baaffb03ace3c1dcef3dbedd
Keeeparis <vladimir1trotsenko@gmail.com>
2021-11-17 17:38:15 +0600

    authProvider context out of App.js

--- src/App.js
+++ src/App.js
@@ -2,72 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import './styles/index.scss'
 import 'materialize-css'
-import {AuthContext} from './context/index'
-import { useEffect, useState } from "react";
-import ApiRequest from "./API/ApiRequest";
+import AuthProvider from "./components/AuthProvider";
 
 function App() {
-    const [cards, setCards] = useState({
-        city: {
-            name: 'Weather',
-            items: []
-        }
-    })
-    const [tasks, setTasks] = useState({
-        toDo: {
-            name: { en: 'To do', ru: 'Задачи' },
-            items: []
-        },
-        inProgress: {
-            name: { en: 'In progress', ru: 'В процессе' },
-            items: []
-        },
-        Done: {
-            name: { en: 'Done', ru: 'Выполнено' },
-            items: []
-        }
-    })
-    const [isLoaded, setIsLoaded] = useState(true)
-    const [lang, setLang] = useState('')
-
-    useEffect(() => {
-        if (localStorage.getItem('vtru_cards')) {
-            setCards(JSON.parse(localStorage.getItem('vtru_cards')))
-        }
-        if (localStorage.getItem('vtru_tasks')) {
-            setTasks(JSON.parse(localStorage.getItem('vtru_tasks')))
-        }
-        if (localStorage.getItem('vtru_lang')) {
-            setLang(JSON.parse(localStorage.getItem('vtru_lang')))
-        }
-        setIsLoaded(false)
-    }, [])
-
-    useState(() => {
-        const refreshUponReload = async () => {
-            const data = JSON.parse(localStorage?.getItem('vtru_cards'))
-            if (!data.city.items.length) return
-            
-            const cityColumn = {...data.city}
-            const cityList = cityColumn.items
-
-            const refsreshedCards = await Promise.all(cityList.map(async card => {
-                const response = await ApiRequest.getData({lat: card.location.lat, lng: card.location.lon})
-                return {...response, lastUpdated: Date.now(), city: card.city, id: Date.now().toString()}
-            }))
-            setCards({...data, city: {...cityColumn, items: refsreshedCards}})
-        }
-        refreshUponReload()
-    }, [])
-
     return (
-        <AuthContext.Provider
-            value={{cards, setCards, tasks, setTasks, isLoaded, lang, setLang}}
-        >
+        <AuthProvider>
             <BrowserRouter>
                 <AppRouter />
             </BrowserRouter>
-        </AuthContext.Provider>
+        </AuthProvider>
     );
 }
 
