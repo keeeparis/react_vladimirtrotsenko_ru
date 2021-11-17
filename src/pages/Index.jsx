30b8ffc74ca5d26bba31bfdd5c7ed561ed19e678
Keeeparis <vladimir1trotsenko@gmail.com>
2021-11-17 17:40:15 +0600

    index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {useDictionary} from '../hooks/dictionary.hook'
+import mainImage from '../media/images/main.jpg'
+import {AuthContext} from '../context/index'
 
 export default function Index() {
+    const {lang} = useContext(AuthContext)
+    const words = useDictionary(lang)
+
     return (
         <div className='content'>
-            <h1>This index Page. Welcome</h1>
+            <h2>{words.welcome}!</h2>
+            <div className='main'>
+                <img src={mainImage} alt="" />
+                <div className='text'>
+                    Меня зовут Владимир. Я такой-такой. Я такой-такой. Я такой-такой. Я такой-такой. Я такой-такой. Я такой-такой. Я такой-такой.
+                    А еще я какой-какой-то. А еще я какой-какой-то. А еще я какой-какой-то. А еще я какой-какой-то. А еще я какой-какой-то. 
+                    А еще я какой-какой-то. А еще я какой-какой-то. А еще я какой-какой-то. А еще я какой-какой-то. А еще я какой-какой-то. 
+                </div>
+            </div>
         </div>
     )
 }
